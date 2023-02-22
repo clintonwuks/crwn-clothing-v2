287fc7d02ebdeb8ef5f64a6d998f9e2d8c55237b
clint <clintonwuks@gmail.com>
2023-02-22 13:13:24 +0000

    fixed reload issue

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useCallback, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { UserContext } from "../../context/user.context";
+
 import SignUpForm from "../../component/sign-up-form/sign-up-form.component";
 import SignInForm from "../../component/sign-in-form/sign-in-form.component";
-import {AuthenticationContainer} from "./authentication.styles";
+import { AuthenticationContainer } from "./authentication.styles";
 
 const Authentication = () => {
+  const { currentUser } = useContext(UserContext);
+
+  const navigate = useNavigate();
+  const navigate1 = useCallback(
+    () => (currentUser ? navigate("/") : navigate("/auth")),
+    [currentUser]
+  );
+
+  useEffect(() => {
+    navigate1();
+  }, [navigate1]);
 
   return (
     <AuthenticationContainer>
